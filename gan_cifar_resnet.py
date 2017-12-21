a7ad03f2d08ca09d450ace2d9f484a6cae9df853
lukovnikov <lukovnikov@outlook.com>
2017-12-21 23:28:04 +0100

    merged IS and FID

--- gan_cifar_resnet.py
+++ gan_cifar_resnet.py
@@ -433,7 +433,7 @@ def run(mode="wgan-gp", dim_g=128, dim_d=128, critic_iters=5,
             all_samples = all_samples.reshape((-1, 3, 32, 32)).transpose(0,2,3,1)
             print("getting IS and FID")
             _inception_score, _fid_score = fid.calc_IS_and_FID(all_samples, session, (mu_real, sigma_real), 100, True)
-            _inception, _inception_std = inception_score
+            _inception, _inception_std = _inception_score
 
             # _inception_score_check = lib.inception_score.get_inception_score(list(all_samples), sess=session)
             # print(_inception_score, _inception_score_check)
