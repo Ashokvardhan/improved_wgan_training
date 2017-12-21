f0482af7e4f678555a0405a46a4c8744b99cdf38
lukovnikov <lukovnikov@outlook.com>
2017-12-21 19:31:27 +0100

    added fid

--- gan_cifar_resnet.py
+++ gan_cifar_resnet.py
@@ -104,7 +104,7 @@ def run(mode="wgan-gp", dim_g=128, dim_d=128, critic_iters=5,
         allimages = allimages.reshape((-1, 3, 32, 32)).transpose(0,2,3,1)
         # images = list(allimages)
         images = allimages
-        print("%d images found and loaded: {}" % len(images, images.shape))
+        print("%d images found and loaded: {}" % len(images), images.shape)
 
         print("create inception graph..")
         fid.create_inception_graph(inception_path)  # load the graph into the current TF graph
